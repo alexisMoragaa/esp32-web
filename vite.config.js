e7b8c0d2d797a68bc9894bbc37e162a781ec8196
AMG <alexis.moraga.gallardo@gmail.com>
2026-09-03 09:28:05 -0400

    Prueba en viteconfig

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
  * @returns {import('vite').UserConfig}
  */
 export default defineConfig({
-  base: '/esp32-web/',
+  // base: '/esp32-web/',
   build: {
     rollupOptions: {
       input: {
